94b8c66d647fb20302fd66e69e5383548db23857
hun5w <1076813765@qq.com>
2025-05-20 22:49:46 +0800

    密码接口无法使用，修改了相关代码

--- vue-music/src/api/auth.js
+++ vue-music/src/api/auth.js
@@ -1,22 +1,15 @@
-import request from "@/utils/request";
-
-// 手机 + 密码 登录
-export function loginWithPassword(phone, password) {
-  return request.get("/login/cellphone", {
-    params: { phone, password }
-  });
-}
+import request from '@/utils/request';
 
 // 发送验证码
 export function sendCaptcha(phone) {
-  return request.get("/captcha/sent", {
+  return request.get('/captcha/sent', {
     params: { phone }
   });
 }
 
-// 验证验证码
-export function verifyCaptcha(phone, code) {
-  return request.get("/captcha/verify", {
-    params: { phone, captcha: code }
+// 验证验证码登录
+export function verifyCaptcha(phone, captcha) {
+  return request.get('/captcha/verify', {
+    params: { phone, captcha }
   });
 }
