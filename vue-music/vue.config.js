93298dc1a1519a5f3f928735846a8a2d3d11d12b
hun5w <1076813765@qq.com>
2025-05-21 14:13:16 +0800

    删除了没有用到的跨域代理配置

--- vue-music/vue.config.js
+++ vue-music/vue.config.js
@@ -25,17 +25,5 @@ module.exports = {
     ],
   },
 
-  // 新增 devServer 代理配置，解决跨域
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'https://netease-cloud-music-api.vercel.app', // 目标API地址，换成你的代理地址
-        changeOrigin: true,
-        secure: false,
-        pathRewrite: {
-          '^/api': '',  // 去掉请求路径中的 /api 前缀
-        },
-      },
-    },
-  },
+
 };
