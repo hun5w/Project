9a1b7093b722df6e45e4608a5e85c137a9fa50bb
hun5w <1076813765@qq.com>
2025-05-21 14:16:09 +0800

    配置的本地静态用户数据

---
+++ vue-music/src/data/users.js
@@ -0,0 +1,17 @@
+
+export const localUsers = [
+  {
+    phone: '18970952426',
+    password: '123456',
+    nickname: '测试用户',
+    level: 5,
+    playlists: ['我喜欢的音乐', '经典老歌']
+  },
+  {
+    phone: '123',
+    password: '123',
+    nickname: '用户b',
+    level: 3,
+    playlists: ['学习专用', '冥想音乐']
+  }
+]
