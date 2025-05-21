c88dc1571aacf60711d3a1d889a47eaeeb463214
hun5w <1076813765@qq.com>
2025-05-21 20:04:13 +0800

    修改了在游客状态下的逻辑

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <router-view /> <!-- 页面内容区域 -->
-    <van-tabbar  
+
+    <!-- 只在非登录页显示底部导航栏 -->
+    <van-tabbar
+      v-if="!isLoginPage"
       route
       active-color="#ec4141"
-      inactive-color="#999"> 
+      inactive-color="#999"
+    >
       <van-tabbar-item to="/home" icon="home-o">推荐</van-tabbar-item>
       <van-tabbar-item to="/finding" icon="music-o">发现</van-tabbar-item>
       <van-tabbar-item to="/note" icon="chat-o">笔记</van-tabbar-item>
@@ -16,12 +20,13 @@
 <script>
 export default {
   name: "App",
-  data() {
-    return {
-      active: 0, // 当前激活索引，但 route 模式会自动处理
-    };
-  },
-};
+  computed: {
+    // 判断当前路径是否为登录页
+    isLoginPage() {
+      return this.$route.path === '/login'
+    }
+  }
+}
 </script>
 
 <style>
