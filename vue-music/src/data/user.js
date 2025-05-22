5539a28a4fc1f4e0ab939651656ab3a0c9f88587
hun5w <1076813765@qq.com>
2025-05-22 12:22:44 +0800

    默认用户数据

---
+++ vue-music/src/data/user.js
@@ -0,0 +1,16 @@
+export const defaultUsers = [
+  {
+    id: 1,
+    username: 'testuser',
+    password: '123456',
+    nickname: '测试用户',
+    avatarUrl: 'https://img.icons8.com/color/96/user.png',
+    signature: '我是一名测试用户',
+    level: 5,
+    vipType: 1,
+    listenSongs: 1200,
+    playlistCount: 3,
+    follows: 100,
+    fans: 55
+  }
+]
