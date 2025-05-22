5d49dfef48272282f28644a3c118d89a52941326
hun5w <1076813765@qq.com>
2025-05-22 20:46:17 +0800

    优化路由配置格式，调整代码缩进和空格

--- vue-music/src/router/index.js
+++ vue-music/src/router/index.js
@@ -1,46 +1,56 @@
-import {createRouter,createWebHashHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-    history:createWebHashHistory(),
-    routes:[
-        {
-            path:'/',
-            redirect:'/login'
-        },
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/login/LoginIndex.vue')
+    },
+    {
+      path: '/register',
+      component: () => import('@/views/login/RegisterIndex.vue')
+    },
+    {
+      path: '/mine',
+      component: () => import('../views/mine/MineIndex.vue')
+    },
+    {
+      path: '/finding',
+      component: () => import('../views/finding/FindingIndex.vue')
+    },
 
-        {
-            
-            path:'/login',
-            component:()=>import('../views/login/LoginIndex.vue')
-        },
-        {
-            path: '/register',
-            component: () => import('@/views/login/RegisterIndex.vue')
-        },
+    // 分类歌单列表页，使用query参数或params都可以，这里按你之前的用法：
+    {
+      path: '/playlist',
+      component: () => import('../views/finding/components/CategoryDetail.vue')
+    },
 
-        {
-            
-            path:'/mine',
-            component:()=>import('../views/mine/MineIndex.vue')
-        },
+    // 歌单详情页，展示歌单中的歌曲列表
+    {
+      path: '/playlist/detail/:playlistId',
+      component: () => import('../components/SongList.vue')
+    },
 
-        {
-            path:'/finding',
-            component:()=>import('../views/finding/FindingIndex.vue')
-        },
+    // 歌曲播放详情页
+    {
+      path: '/song/:songId',
+      component: () => import('../components/SongDetail.vue')
+    },
 
-        {
-            path:'/note',
-            component:()=>import('../views/note/NoteIndex.vue')
-        },
-
-        {
-            path:'/home',
-            component:()=>import('../views/home/HomeIndex.vue')
-        },
-
-        
-    ]
+    {
+      path: '/note',
+      component: () => import('../views/note/NoteIndex.vue')
+    },
+    {
+      path: '/home',
+      component: () => import('../views/home/HomeIndex.vue')
+    },
+  ]
 })
 
 export default router
