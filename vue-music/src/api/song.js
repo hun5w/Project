68fa45b1a21fb835053019cd35d93fc5d4ca6517
hun5w <1076813765@qq.com>
2025-05-25 23:20:41 +0800

    更新 getSongUrl 函数，修改请求 URL 并添加 level 参数以支持不同音质

--- vue-music/src/api/song.js
+++ vue-music/src/api/song.js
@@ -8,9 +8,17 @@ export function getSongDetail(songId) {
   })
 }
 
-export function getSongUrl(songId) {
+export function getSongUrl(songId, level = 'standard') {
   return request({
-    url: '/song/url',
+    url: '/song/url/v1',
+    method: 'get',
+    params: { id: songId, level }
+  })
+}
+
+export function checkMusic(songId) {
+  return request({
+    url: '/check/music',
     method: 'get',
     params: { id: songId }
   })
