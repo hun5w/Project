2765aa773fa0f387ae354105d57941c07c41f9ee
hun5w <1076813765@qq.com>
2025-05-25 22:41:32 +0800

    新增播放器状态管理功能，集成 Pinia 以支持播放列表和当前索引的管理

---
+++ vue-music/src/store/player.js
@@ -0,0 +1,55 @@
+import { defineStore } from 'pinia'
+
+export const usePlayerStore = defineStore('player', {
+  state: () => ({
+    playlist: [],
+    currentIndex: 0,
+    playing: false,
+    currentTime: 0,
+    duration: 0,
+  }),
+  getters: {
+    currentSongId: (state) => state.playlist[state.currentIndex]?.id || null,
+  },
+  actions: {
+    setPlaylist(list) {
+      this.playlist = list
+      this.currentIndex = 0
+      this.currentTime = 0
+      this.playing = false
+      this.duration = 0
+    },
+    setCurrentIndex(index) {
+      if (index >= 0 && index < this.playlist.length) {
+        this.currentIndex = index
+        this.currentTime = 0
+        this.duration = 0
+      }
+    },
+    setPlaying(flag) {
+      this.playing = flag
+    },
+    togglePlay() {
+      this.playing = !this.playing
+    },
+    setCurrentTime(time) {
+      this.currentTime = time
+    },
+    setDuration(duration) {
+      this.duration = duration
+    },
+    seek(time) {
+      this.currentTime = time
+    },
+    next() {
+      this.currentIndex = (this.currentIndex + 1) % this.playlist.length
+      this.currentTime = 0
+      this.duration = 0
+    },
+    prev() {
+      this.currentIndex = (this.currentIndex - 1 + this.playlist.length) % this.playlist.length
+      this.currentTime = 0
+      this.duration = 0
+    },
+  },
+})
