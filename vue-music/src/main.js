8bb1768bfe36af45b80fee3cee6244dc3824ce37
hun5w <1076813765@qq.com>
2025-05-25 15:34:33 +0800

    新增 Pinia 状态管理库以支持状态管理功能

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -4,8 +4,10 @@ import './assets/base.css'
 import 'amfe-flexible'
 import router from './router/index'
 import { Button} from 'vant'
+import { createPinia } from 'pinia'
 
-
+const pinia = createPinia()
 const app = createApp(App)
-app.use(router).use(Button)
+
+app.use(router).use(Button).use(pinia)
 app.mount('#app')
